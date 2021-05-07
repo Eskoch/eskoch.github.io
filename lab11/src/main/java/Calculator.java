8da658f54864189e02d5898f11bd10acc010579c
Eskoch <abebeeskender@gmial.com>
2021-05-06 20:27:49 -0500

    lab11

---
+++ lab11/src/main/java/Calculator.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Calculator extends HttpServlet {
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		PrintWriter out = res.getWriter();
+		
+		if(!req.getParameter("num1").isBlank() && !req.getParameter("num2").isBlank() && 
+				!req.getParameter("num3").isBlank() && !req.getParameter("num4").isBlank()) {
+			int num1 = Integer.parseInt(req.getParameter("num1"));
+			int num2 = Integer.parseInt(req.getParameter("num2"));
+			int num3 = Integer.parseInt(req.getParameter("num3"));
+			int num4 = Integer.parseInt(req.getParameter("num4"));
+			out.println(num1 + " + " + num2 + " = " + (num1 + num2)); 
+			out.println("\n" + num3 + " * " + num4 + " = " + num3 * num4);
+		} 
+		else if (req.getParameter("num3").isBlank()) {
+			int num1 = Integer.parseInt(req.getParameter("num1"));
+			int num2 = Integer.parseInt(req.getParameter("num2"));
+			out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+		} 
+		else if(req.getParameter("num1").isBlank()) {
+			int num3 = Integer.parseInt(req.getParameter("num3"));
+			int num4 = Integer.parseInt(req.getParameter("num4"));
+			out.println("\n" + num3 + " * " + num4 + " = " + num3 * num4);
+		} 
+		else {
+			out.println("Invalid Input");
+		}
+			
+	}
+
+}
---
+++ lab11/src/main/java/Calculator2.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Calculator2 extends HttpServlet{
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		
+		if(!req.getParameter("num21").isBlank() && !req.getParameter("num22").isBlank() && 
+				!req.getParameter("num23").isBlank() && !req.getParameter("num24").isBlank()) {
+			int num21 = Integer.parseInt(req.getParameter("num21"));
+			int num22 = Integer.parseInt(req.getParameter("num22"));
+			int num23 = Integer.parseInt(req.getParameter("num23"));
+			int num24 = Integer.parseInt(req.getParameter("num24"));
+			int sum = num21 + num22;
+			int mult = num23 * num24;
+			out.println("<input name=\"num21\" value="+num21+"> + <input name=\"num22\" value="+num22+"> = <input name=\"sum\" value="+sum+" readonly><br>");
+			out.println("<input name=\"num23\" value="+num23+"> * <input name=\"num24\" value="+num24+"> = <input name=\"sum\" value="+mult+" readonly>");
+
+		} 
+		else if (req.getParameter("num23").isBlank()) {
+			int num21 = Integer.parseInt(req.getParameter("num21"));
+			int num22 = Integer.parseInt(req.getParameter("num22"));
+			int sum = num21 + num22;
+			out.println("<input name=\"num21\" value="+num21+"> + <input name=\"num22\" value="+num22+"> = <input name=\"sum\" value="+sum+" readonly><br>");
+		} 
+		else if(req.getParameter("num21").isBlank()) {
+			int num23 = Integer.parseInt(req.getParameter("num23"));
+			int num24 = Integer.parseInt(req.getParameter("num24"));
+			int mult = num23 * num24;
+			out.println("<input name=\"num23\" value="+num23+"> * <input name=\"num24\" value="+num24+"> = <input name=\"sum\" value="+mult+" readonly>");
+		} 
+		else {
+			out.println("Invalid Input");
+		}
+	}
+}
