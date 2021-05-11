697cee391e482de6c39f67dc9065261cda31dc85
Eskoch <abebeeskender@gmial.com>
2021-05-10 22:56:13 -0500

    lab15-1

--- lab12/src/main/java/Game.java
+++ lab12/src/main/java/Game.java
@@ -14,31 +14,31 @@ public class Game extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		HttpSession session = req.getSession();
 		Quiz quiz;
-		
-		if(session.getAttribute("quiz") ==  null){
-            quiz =  new Quiz();
-            session.setAttribute("quiz",quiz);
-        } else {
-            String ans = req.getParameter("result");
-            quiz = (Quiz)session.getAttribute("quiz");
-            quiz.checkAnswer(ans);
-            session.setAttribute("quiz", quiz);
-        }
-		
-		res.setContentType("text/html");
+
+				if(session.getAttribute("quiz") ==  null){
+	            quiz =  new Quiz();
+	            session.setAttribute("quiz",quiz);
+	      } else {
+	            String ans = req.getParameter("result");
+	            quiz = (Quiz)session.getAttribute("quiz");
+	            quiz.checkAnswer(ans);
+	            session.setAttribute("quiz", quiz);
+	      }
+
+				res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         String question = quiz.getQuestion();
-		
+
         if(question == null){
             out.println("<p>Game End! Score: "+quiz.getScore()+ "</p>");
-        }else {
-		
-			out.println("<h1>Welcome to the Number Quiz</h1>  <form action=\"play\" method=\"post\"> Your current score is " + quiz.getScore() + " .<br> Guess the next number in the sequence. <br>" + question + " Your answer: <input type='text' name='result'> <input type='submit'></form>");		
+        } else {
+
+			out.println("<h1>Welcome to the Number Quiz</h1>  <form action=\"play\" method=\"post\"> Your current score is " + quiz.getScore() + " .<br> Guess the next number in the sequence. <br>" + question + " Your answer: <input type='text' name='result'> <input type='submit'></form>");
         }
-			
+
 	}
-	
+
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        this.doPost(req, res);
+        doPost(req, res);
     }
 }
---
+++ lab15-1/guestListAjax/src/main/webapp/resources/js/guests.js
@@ -0,0 +1,27 @@
+$(function() {
+    $("#send").click(updateGuests);
+});
+
+function updateGuests() {
+    var first = $("#first").val();
+    var last = $("#last").val();
+
+    $.ajax("guest.ajax", {
+		"type": "post",
+		"data": {
+        	"first": first,
+                "last": last
+		}
+    }).done(displayGuests);
+}
+
+function displayGuests(data) {
+    // var guestList = "You need to modify this method to display the updated guest list.  Remember to build the entire list before adding it to the DOM.";
+    let guestList = "";
+    $.each(data, function(fName, lName){
+      let str = "<p>" + fName.first + " " + lName.last + "</p>";
+      guestList += str;
+    });
+    $("#guestList").html(guestList);
+
+}
